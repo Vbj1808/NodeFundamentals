944890b61c7b6ec0fd49f58ceae1f5aa54ba98b9
Vbj <ijalab1808@gmail.com>
2020-05-23 08:30:48 +0530

    Express Generator

--- node-express/index.js
+++ node-express/index.js
@@ -5,6 +5,11 @@ const express = require('express'),
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+
+const promoRouter = require('./routes/promoRouter');
+
+const leaderRouter = require('./routes/leaderRouter');
+
     
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +18,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter.router);
+app.use('/leadership', leaderRouter.router);
+
 
 
 
