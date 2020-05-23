04db18316c3bcd043445e0c30f188de6b71ab2e6
Vbj <ijalab1808@gmail.com>
2020-05-23 11:10:45 +0530

    Node mongodb example 1

---
+++ node-mongo/index.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'conFusion';
+
+MongoClient.connect(url, (err,client) => {
+    assert.equal(err,null);
+
+    console.log('Connected Correctly to Server');
+
+    const db = client.db(dbname);
+    const collection = db.collection("dishes");
+    collection.insertOne({"name" : "Uthappizza", "description": "testing"},
+    (err,result) =>{
+        assert.equal(err,null);
+        console.log("After insert : \n");
+        console.log(result.ops);
+
+        collection.find({}).toArray((err,docs) => {
+            assert.equal(err,null);
+
+            console.log("Found:\n");
+            console.log(docs);
+
+            db.dropCollection("dishes",(err,result) => {
+                assert.equal(err,null);
+                client.close();
+            });
+        });
+    });
+});
